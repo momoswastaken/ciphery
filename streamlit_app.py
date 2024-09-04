1de616edb08b569f21f0adc159fbe22fe6266f5f
Muhammad Parkar <parkarmuhammad275@gmail.com>
2024-09-04 21:12:39 +0530

    sih

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,25 +20,54 @@ tab1,tab2, = st.tabs(["📂 Upload a file", "⚡ Enter text manually"])
 
 with tab1:
    
-   uploaded_file = st.file_uploader("Upload your document", type=["pdf", "docx", "txt"])
+   uploaded_file = st.file_uploader("Upload your document", type=["pdf", "docx", "txt","md"])
    
    if not uploaded_file:
        st.info("Please upload your document to continue", icon="📂")
    else:
+       
         success_message = st.success("File uploaded successfully!", icon="✅")
-        time.sleep(3)
-        success_message.empty()
-           
+        analyze_button = st.button("Analyze 🕵🏼")
+        analyze_message = st.empty()
+        
+        if analyze_button:
+            
+            analyze_message.write("Analyzing...🔍")
+            time.sleep(2)  
+            analyze_message.empty()
+
+            analyzed_algo = st.subheader("Encryption Algorithm : AES encryption algorithm")
+            st.image("./images/aes.jpg")
+                 
 with tab2:
     
     cipher_text = st.text_area(
     "Ciphered Text",
-    placeholder="Enter your ciphered text here. Example:\
-        ",
+    placeholder="Enter your ciphered text here :" +
+    "\n4D 0F D0 D2 A0 09 F5 10 E0 8A 30 06 4D 53 A4 1F 63 4A 90 29 4D 0F D0 D2 A0 09 F5 10 E0 8A 30 06 4D 53 A4 1F 63 4A 90 29",
     height=150,  # Adjust height as needed
     )
     
-    st.info("Please add your cipher text to continue.", icon="🗝️")
+    info_placeholder = st.empty()
+    
+    if not cipher_text.strip():  
+        info_placeholder.info("Please add your cipher text to continue.", icon="🗝️")
+    else:
+        info_placeholder.empty()
+    
+        analyze_button = st.button("Analyze 🕵🏼")
+        analyze_message = st.empty()
+            
+        if analyze_button:   
+            analyze_message.write("Analyzing...🔍")
+            time.sleep(2)  
+
+            analyze_message.empty()
+
+            analyzed_algo = st.subheader("Encryption Algorithm : AES encryption algorithm")
+            st.image("./images/aes.jpg")
+    
+    
     
     
     
